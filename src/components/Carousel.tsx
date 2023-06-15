e2d553b22b647506ae7028894c187cd91148d0e4
Jackwolf180 <120655678+Jackwolf180@users.noreply.github.com>
2023-06-15 10:15:12 +0530

    Automatic Carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,75 +1,59 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, MutableRefObject, RefObject } from 'react';
 import Image from 'next/image';
-import cimg1 from '../../public/Corouselimage1.png'
-import cimg2 from '../../public/Corouselimage2.png'
-import cimg3 from '../../public/Corouselimage1.jpeg'
+import carouselImagearray from './CarouselImage';
 
 const Carousel = () => {
-  const [slideIndex, setslideIndex] = useState(1);
-  // let count = useRef(0)
-  // setInterval(()=>{
-  //   count.current++
-  // },1000)
-  // let count=0;
-  // setInterval(()=>{count++ 
-  //   if(count==5) {
-  //     count=0};},1000);
+  const [slideIndex, setslideIndex] = useState(0);
+  let timeoutRef = useRef<number| undefined>(undefined) ;
 
-  const showSlide=(n:number)=>{
-    if(n>1){
-      setslideIndex(n-1)
+  const resetTimeout=()=>{
+    if (timeoutRef.current) {
+      window.clearTimeout(timeoutRef.current);
     }
-    else{
-      setslideIndex(slideIndex+n)
-    }
-  if (slideIndex > 3) { setslideIndex(1) }
-  if (slideIndex < 1) { setslideIndex(3) }
-    // let timeout=setTimeout(()=>{showSlide(1) 
-    // count++},5000)
-    // if(count<=5){
-    //   clearTimeout(timeout);
-    // }
   }
 
-  // useEffect(() => {
-  //   count.current=0;
-    
-  // }, [slideIndex]);
+  if (slideIndex > carouselImagearray.length-1) { setslideIndex(0) }
+  if (slideIndex < 0) { setslideIndex(carouselImagearray.length-1) }
+
+  useEffect(() => {
+      resetTimeout()
+    timeoutRef.current = window.setTimeout(
+    () =>{
+      setslideIndex((prevIndex) =>  prevIndex === carouselImagearray.length ? 0: prevIndex + 1)},500000);
+  
+    return () => {
+      resetTimeout();
+    };
+  }, [slideIndex]);
 
 
-  return (
+
+    return (
     <div className='carouselMain'>
       {/* <!-- Slideshow container --> */}
       <div className="slideshow-container">
 
-        {/* <!-- Full-width images with number and caption text --> */}
-        {slideIndex == 1 && <div className="myslide fade">
+        {carouselImagearray.map((info, index) => {
+          return (slideIndex == index) && <div className="myslide fade">
 
-          <Image src={cimg1} className='image' alt="Caorusel Image 1" />
-          <div className="text">Caption Text</div>
-        </div>}
-        {slideIndex == 2 && <div className="myslide fade">
-          <Image src={cimg2} className='image' alt="Caorusel Image 2" />
-          <div className="text">Caption Two</div>
-        </div>}
+            <Image src={info.url} className='image' alt={info.Alernaivetext} width={1024} height={1024}/>
+            <div className="text">{info.Title}</div>
+          </div>
 
-        {slideIndex == 3 && <div className=" myslide fade">
-          <Image src={cimg3} className='image' alt="Caorusel Image 3" />
-          <div className="text">Caption Three</div>
-        </div>}
+        })}
 
         {/* <!-- Next and previous buttons --> */}
-        <a className="prev" onClick={() => { showSlide(-1) }}>&#10094;</a>
-        <a className="next" onClick={() => { showSlide(1) }}>&#10095;</a>
-             {/* <!-- The dots/circles --> */}
-      <div className='dotContainer'>
-        <span className={`dot ${slideIndex == 1 ? "active" : ""}`} onClick={() => { showSlide(2) }}></span>
-        <span className={`dot ${slideIndex == 2 ? "active" : ""}`} onClick={() => { showSlide(3) }}></span>
-        <span className={`dot ${slideIndex == 3 ? "active" : ""}`} onClick={() => { showSlide(4) }}></span>
-      </div>
+        <a className="prev" onClick={() => { setslideIndex(slideIndex - 1) }}>&#10094;</a>
+        <a className="next" onClick={() => { setslideIndex(slideIndex + 1) }}>&#10095;</a>
+        {/* <!-- The dots/circles --> */}
+        <div className='dotContainer'>
+          {carouselImagearray.map((info, index) => {
+            return <span key={info.Title} className={`dot ${slideIndex == index ? "active" : ""}`} onClick={() => { setslideIndex(index) }}></span>
+          })}
+        </div>
       </div>
     </div>
-  );
+    );
 }
 
-export default Carousel;
+    export default Carousel;
---
+++ src/components/CarouselImage.tsx
@@ -0,0 +1,37 @@
+import image_1 from '../../public/image 1.png'
+import image_2 from '../../public/image 2.webp'
+import image_3 from '../../public/image 3.jpg'
+import image_4 from '../../public/image 4.jpeg'
+import image_5 from '../../public/image 5.jpg'
+
+
+let carouselImagearray = [
+    {
+        "Title": "First Image",
+        "Alernaivetext": "This is the First image of the carousel",
+        "url": `${image_1.src}`
+    },
+    {
+        "Title": "Second Image",
+        "Alernaivetext": "This is the Second image of the carousel",
+        "url": `${image_2.src}`
+    },
+    {
+        "Title": "Third Image",
+        "Alernaivetext": "This is the Third image of the carousel",
+        "url": `${image_3.src}`
+    },
+    {
+        "Title": "Forth Image",
+        "Alernaivetext": "This is the Forth image of the carousel",
+        "url": `${image_4.src}`
+    },
+    {
+        "Title": "Fifth Image",
+        "Alernaivetext": "This is the Fifth image of the carousel",
+        "url": `${image_5.src}`
+    },
+]
+
+
+export default carouselImagearray;
