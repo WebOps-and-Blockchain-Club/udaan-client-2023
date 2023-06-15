ce793f9cd603a86e38f16a76a4c1f9053df22134
thunder-volt <ishu114407@gmail.com>
2023-06-15 15:09:29 +0530

    merge conflict solved

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Navbar from '@/components/Navbar'
+import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer'
 import Styles from '@/styles/Home.module.css'
 import Script from 'next/script'
